2d57300d9b1cad4aae492bf0c8089d072d598a3c
XulunLuo <xulunl@andrew.cmu.edu>
2026-02-18 13:24:14 -0500

    Update the activation

--- HW2/HW2P1/hw2p1_handout/mytorch/nn/activation.py
+++ HW2/HW2P1/hw2p1_handout/mytorch/nn/activation.py
@@ -34,6 +34,16 @@ class Sigmoid:
     Define 'backward' function.
     Read the writeup (Hint: Sigmoid Section) for further details on Sigmoid forward and backward expressions.
     """
+    def forward(self, Z):
+        self.A = 1 / (1 + np.exp(-Z))
+
+        return self.A
+    
+    def backward(self, dLdA):
+        dAdZ = self.A * (1 - self.A)
+        dLdZ = dLdA * dAdZ
+
+        return dLdZ
 
 
 class Tanh:
@@ -46,6 +56,18 @@ class Tanh:
     Read the writeup (Hint: Tanh Section) for further details on Tanh forward and backward expressions.
     """
 
+    def forward(self, Z):
+        self.A = np.tanh(Z)
+
+        return self.A
+    
+    def backward(self, dLdA):
+        dAdZ = 1 - self.A ** 2
+        dLdZ = dLdA * dAdZ
+
+        return dLdZ
+
+
 
 class ReLU:
     """
@@ -56,6 +78,17 @@ class ReLU:
     Define 'backward' function.
     Read the writeup (Hint: ReLU Section) for further details on ReLU forward and backward expressions.
     """
+
+    def forward(self, Z):
+        self.A = np.maximum(0, Z)
+
+        return self.A
+    
+    def backward(self, dLdA):
+        dAdZ = (self.A > 0).astype(float)
+        dLdZ = dLdA * dAdZ
+        
+        return dLdZ
 
 
 class GELU:
@@ -69,6 +102,21 @@ class GELU:
     Note: Feel free to save any variables from gelu.forward that you might need for gelu.backward.
     """
 
+    def forward(self, Z):
+        self.Z = Z
+        self.Phi = Z * 0.5 * (1 + np.erf(Z / np.sqrt(2)))
+        self.A = Z * self.Phi
+
+        return self.A
+    
+    def backward(self, dLdA):
+        phi = (1 / np.sqrt(2 * np.pi)) * np.exp(-0.5 * self.Z ** 2)
+        dAdZ = self.Phi + self.Z * phi
+
+        dLdZ = dLdA * dAdZ
+
+        return dLdZ
+
 
 class Swish:
     """
@@ -79,6 +127,25 @@ class Swish:
     Define 'backward' function.
     Read the writeup (Hint: Swish Section) for further details on Swish forward and backward expressions.
     """
+    def __init__(self, beta=1.0):
+        self.beta = beta
+    
+    def forward(self, Z):
+        self.Z = Z 
+        self.sigmoid = 1 / (1 + np.exp(-self.beta * Z))
+        self.A = Z * self.sigmoid
+
+        return self.A 
+    
+    def backward(self, dLdA):
+        sig = self.sigmoid
+        dAdZ = sig * (1 + self.beta * self.Z * (1 - sig))
+
+        # Grdient sum over all elements
+        self.dLdbeta = np.sum(dLdA * self.Z ** 2 * sig * (1 - sig))
+        dLdZ = dLdA * dAdZ
+    
+        return dLdZ
 
 
 class Softmax:
@@ -97,30 +164,37 @@ class Softmax:
         It will use an entire row of Z to compute an output element.
         Note: How can we handle large overflow values? Hint: Check numerical stability.
         """
-        self.A = None  # TODO
-        raise NotImplementedError  # TODO - What should be the return value?
+        Z_stable = Z - np.max(Z, axis=1, keepdims=True)
+        exp_Z = np.exp(Z_stable)
+        self.A = exp_Z / np.sum(exp_Z, axis=1, keepdims=True)  # TODO
+        return self.A  # TODO - What should be the return value?
 
     def backward(self, dLdA):
         # Calculate the batch size and number of features
-        N = None  # TODO
-        C = None  # TODO
+        N = self.A.shape[0]  # Batch size
+        C = self.A.shape[1]  # Number of classes
 
         # Initialize the final output dLdZ with all zeros. Refer to the writeup and think about the shape.
-        dLdZ = None  # TODO
+        dLdZ = np.zeros((N, C))  # TODO
 
         # Fill dLdZ one data point (row) at a time.
         for i in range(N):
             # Initialize the Jacobian with all zeros.
             # Hint: Jacobian matrix for softmax is a _×_ matrix, but what is _ here?
-            J = None  # TODO
+            J = np.zeros((C, C))  # TODO
 
             # Fill the Jacobian matrix, please read the writeup for the conditions.
             for m in range(C):
                 for n in range(C):
-                    J[m, n] = None  # TODO
+                    if m == n:
+                        J[m, n] = self.A[i, m] * (1 - self.A[i, m])
+                    else:
+                        J[m, n] = -self.A[i, m] * self.A[i, n]
+                        
 
             # Calculate the derivative of the loss with respect to the i-th input, please read the writeup for it.
             # Hint: How can we use (1×C) and (C×C) to get (1×C) and stack up vertically to give (N×C) derivative matrix?
-            dLdZ[i, :] = None  # TODO
+            # So for (1×C) @ (C×C) we will get (1×C), and we will store it as row i
+            dLdZ[i, :] = dLdA[i, :] @ J  # TODO
 
-        raise NotImplementedError  # TODO - What should be the return value?
+        return dLdZ  # TODO - What should be the return value?
